155dc887da9907fbddc813c1528589f3c08af25a
Arthur <arthurleit10@gmail.com>
2026-03-06 17:03:08 -0300

    criando a responsividade

---
+++ dist/contagem_aniversario.f75de5e1.js
@@ -0,0 +1,24 @@
+AOS.init();
+document.addEventListener("DOMContentLoaded", function() {
+    setInterval(function() {
+        const evento = new Date("Marc, 18, 2026");
+        const timeStampEvent = evento.getTime();
+        const hoje = new Date();
+        const timeStampHoje = hoje.getTime();
+        const diferencaTimeStamp = timeStampEvent - timeStampHoje;
+        const dias = 86400000;
+        const horas = 3600000;
+        const minutos = 60000;
+        const difeDias = Math.floor(diferencaTimeStamp / dias);
+        const difeHoras = Math.floor(diferencaTimeStamp % dias / horas);
+        const difeMinutos = Math.floor(diferencaTimeStamp % horas / minutos);
+        const difeSegundos = Math.floor(diferencaTimeStamp % minutos / 1000);
+        console.log("Dias:", difeDias);
+        console.log("Horas:", difeHoras);
+        console.log("Minutos:", difeMinutos);
+        console.log("Segundos:", difeSegundos);
+        document.getElementById('exibeNaTela').innerHTML = `${difeDias}d ${difeHoras}h ${difeMinutos}m ${difeSegundos}s`;
+    });
+});
+
+//# sourceMappingURL=contagem_aniversario.f75de5e1.js.map
